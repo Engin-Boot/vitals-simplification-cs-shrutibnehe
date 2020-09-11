4ce04f370a92790d8f81eb82fe7c5e6099fee7e6
shrutibnehe <56735035+shrutibnehe@users.noreply.github.com>
2020-09-11 13:39:33 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -1,34 +1,84 @@
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Checker
+namespace VitalsCheker
 {
-    static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
-            return false;
-        }
-        return true;
+    public abstract class Alert
+    {
+        internal abstract void raiseAlert(string vitalname, string message);
+
     }
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
+    internal class SMSAlert : Alert
+    {
+
+        internal override void raiseAlert(string vitalname, string message)
+        {
+            Console.WriteLine("SMS Alert "+vitalname + " is " + message);
         }
     }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
+    internal class SoundAlert : Alert
+    {
+
+        internal override void raiseAlert(string vitalname, string message)
+        {
+            Console.WriteLine("Sound Alert "+vitalname + " is " + message);
         }
     }
-    static int Main() {
-        ExpectTrue(vitalsAreOk(100, 95, 60));
-        ExpectFalse(vitalsAreOk(40, 91, 92));
-        Console.WriteLine("All ok");
+   public class vitals
+    {
+        int upperLimit;
+        int lowerLimit;
+        string vitalName;
+     public vitals(string vitalName, int upperLimit, int lowerLimit)
+        {
+            this.vitalName = vitalName;
+            this.upperLimit = upperLimit;
+            this.lowerLimit = lowerLimit;
+        }
+     public void checkVitalIsOk(Alert alert, int vitalvalue)
+        {
+            Console.WriteLine(vitalvalue + " " + this.upperLimit);
+            if (vitalvalue > this.upperLimit)
+            {
+                alert.raiseAlert(vitalName, "HIGH");
+            }
+            else if (vitalvalue < this.lowerLimit)
+            {
+                alert.raiseAlert(vitalName, "LOW");
+            }
+            else
+            {
+                alert.raiseAlert(vitalName, "NORMAL");
+            }
+        }
+
+    }
+
+
+    public class Checker
+    {
+    
+    static int Main(string[] args)
+    {
+        vitals vital1 = new vitals("BPM", 150, 70);
+        vitals vital2 = new vitals("SPO2", 100, 90);
+        vitals vital3 = new vitals("RespRate", 90, 30);
+
+        SMSAlert smsalert = new SMSAlert();
+        SoundAlert soundalert = new SoundAlert();
+           
+        vital1.checkVitalIsOk(smsalert, 85);
+        vital1.checkVitalIsOk(soundalert, 60);
+        vital3.checkVitalIsOk(soundalert, 95);
+        vital2.checkVitalIsOk(smsalert, 95);
+        vital2.checkVitalIsOk(smsalert, 85);
+        vital3.checkVitalIsOk(smsalert, 70);
+        vital3.checkVitalIsOk(soundalert, 100);
         return 0;
     }
+
+  }
 }
